e4f0df58570d39413f47f463a942aaad9f0411e8
Der Uli im Netz <admin@deruli.de>
2020-06-24 21:40:33 +0200

    update module

--- src/content/modules/absolutify/Absolutify.php
+++ src/content/modules/absolutify/Absolutify.php
@@ -1,40 +1,38 @@
 <?php
 
-function absolutify($html, $baseUrl = null)
-{
-    if (! $baseUrl) {
-        $baseUrl = ModuleHelper::getBaseUrl();
-    }
+function absolutify($html, $baseUrl = null) {
+    $baseUrl = $baseUrl ?? ModuleHelper::getBaseUrl();
+
     $baseUrl = rtrim($baseUrl, '/');
-    
+
     $html = trim($html);
     // this is required to prevent DOMDocument from fucking up the encoding
-	$html = mb_convert_encoding($html, 'HTML-ENTITIES', 'UTF-8');
-	
+    $html = mb_convert_encoding($html, 'HTML-ENTITIES', 'UTF-8');
+
     // Create a new DOM document
     $dom = new DOMDocument('1.0', 'utf-8');
-    
+
     // Parse the HTML. The @ is used to suppress any parsing errors
     // that will be thrown if the $html string isn't valid XHTML.
     @$dom->loadHTML($html, LIBXML_HTML_NOIMPLIED | LIBXML_HTML_NODEFDTD);
     $dom->substituteEntities = true;
 
     $links = $dom->getElementsByTagName('a');
-    
+
     foreach ($links as $link) {
         // prend $baseUrl before all site relative urls
         $href = $link->getAttribute('href');
-        if (! is_url($href)) {
+        if (!is_url($href)) {
             $href = $baseUrl . "/" . ltrim($href, "/");
             $link->setAttribute("href", $href);
         }
     }
     $images = $dom->getElementsByTagName('img');
-    
+
     // prepend $baseUrl before all site relative image srcs
     foreach ($images as $image) {
         $src = $image->getAttribute('src');
-        if (! is_url($src)) {
+        if (!is_url($src)) {
             $src = $baseUrl . "/" . ltrim($src, "/");
             $image->setAttribute("src", $src);
         }
--- src/content/modules/absolutify/tests/AbsolutifyTest.php
+++ src/content/modules/absolutify/tests/AbsolutifyTest.php
@@ -1,18 +1,20 @@
 <?php
+
 use PHPUnit\Framework\TestCase;
+use Spatie\Snapshots\MatchesSnapshots;
 
-class AbsolutifyTest extends TestCase
-{
+class AbsolutifyTest extends TestCase {
+    use MatchesSnapshots;
 
-    public function testAbsolutify()
-    {
+    public function testAbsolutify() {
         $inputHtml = trim(file_get_contents(ModuleHelper::buildModuleRessourcePath("absolutify", "tests/fixtures/input.html")));
-        $expectedHtml = trim(file_get_contents(ModuleHelper::buildModuleRessourcePath("absolutify", "tests/fixtures/expected.html")));
-        
+
         // file_put_contents(ModuleHelper::buildModuleRessourcePath("absolutify", "tests/fixtures/expected.html"), absolutify($inputHtml, "https://www.ulicms.de"));
-        
+
         $outputHtml = \absolutify($inputHtml, "https://www.ulicms.de");
-        $this->assertEquals($expectedHtml, $outputHtml);
+        $this->assertMatchesHtmlSnapshot($inputHtml);
     }
+
 }
+
 ?>
